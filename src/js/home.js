eeef4f3114dfaa7736630dd3eca52d913b6dfca6
Drew McKenzie <mckenzied@my.lanecc.edu>
2025-06-02 15:08:57 -0700

    Minor signature changes

--- src/js/home.js
+++ src/js/home.js
@@ -1,8 +1,7 @@
-// Modified by Drew McKenzie, 5/28/25
+// Created by Drew McKenzie, 5/28/25
 
 import './general';
 const regeneratorRuntime = require("regenerator-runtime");
-
 
 import toastr from 'toastr';
 import 'toastr/toastr.scss';
