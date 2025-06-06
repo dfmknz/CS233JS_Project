2447f1edc5bc2205a1ef2402805d04a49da337c9
Drew McKenzie <mckenzied@my.lanecc.edu>
2025-06-05 17:39:16 -0700

    Attempting to address another prompt error

--- src/js/data.js
+++ src/js/data.js
@@ -4,6 +4,6 @@
 export default function getMoreContext() {
     // place any additional context you want here
     return `
-        
+        Do not respond in JSON format unless requested. The JSON objects are meant to be used for reference ONLY.
     `;
 }
