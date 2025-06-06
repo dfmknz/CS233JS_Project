7562ce10afe871279ace373d4013b9ee2c4b1096
Drew McKenzie <mckenzied@my.lanecc.edu>
2025-06-05 17:25:30 -0700

    comment commit

--- src/js/data.js
+++ src/js/data.js
@@ -1,4 +1,6 @@
 // Created by Drew McKenzie, 6/5/2025
+
+// This can be modified to get customized responses
 export default function getMoreContext() {
     // place any additional context you want here
     return `
